bb518cffb98ad0851a06dba8a71b03d317bae5f4
Andrey Mishchenko <ant1kguild@gmail.com>
2020-03-20 23:40:49 +0300

    refactoring: GetMergeListUseCase refactor rx chain

--- app/src/main/java/com/omertex/mvprx/di/DataModules.kt
+++ app/src/main/java/com/omertex/mvprx/di/DataModules.kt
@@ -6,13 +6,21 @@ import com.omertex.mvprx.data.network.interceptors.NetworkCheckInterceptor
 import com.omertex.mvprx.data.repository.OxTestRepositoryImpl
 import com.omertex.mvprx.domain.repositories.OxTestRepository
 import com.omertex.mvprx.presentation.mvp.global.SchedulersProvider
+import com.omertex.mvprx.presentation.mvp.merge.MergePresenter
 import com.omertex.test.app.data.datasource.PlaceHolderDataSource
 import com.omertex.test.app.data.datasource.UnsplashDataSource
+import com.omertex.test.app.data.model.MergeModelMapper
 import com.omertex.test.app.di.InjectPlaceholder
 import com.omertex.test.app.di.InjectUnsplash
 import com.omertex.test.app.domain.GetMergeListUseCase
 import org.koin.android.ext.koin.androidContext
 import org.koin.dsl.module
+
+
+val utilsModule = module {
+    factory { MergeModelMapper() }
+    factory { SchedulersProvider() }
+}
 
 
 val interceptorModule = module {
@@ -41,8 +49,18 @@ val apiModule = module {
 }
 
 val dataSourceModule = module {
-    single { PlaceHolderDataSource(api = get()) }
-    single { UnsplashDataSource(api = get()) }
+    single {
+        PlaceHolderDataSource(
+            api = get(),
+            schedulersProvider = get()
+        )
+    }
+    single {
+        UnsplashDataSource(
+            api = get(),
+            schedulersProvider = get()
+        )
+    }
 }
 
 val repositoryModule = module {
@@ -54,7 +72,27 @@ val repositoryModule = module {
     }
 }
 
+
 val useCaseModule = module {
-    factory { SchedulersProvider() }
-    factory { GetMergeListUseCase(oxTestRepository = get(), schedulersProvider = get()) }
+    factory {
+        GetMergeListUseCase(
+            oxTestRepository = get(),
+            mergeModelMapper = get(),
+            schedulersProvider = get()
+        )
+    }
 }
+
+val presentersModule = module {
+    factory { MergePresenter() }
+}
+
+val listModules = listOf(
+    utilsModule,
+    interceptorModule,
+    apiModule,
+    dataSourceModule,
+    repositoryModule,
+    useCaseModule,
+    presentersModule
+)
--- app/src/main/java/com/omertex/mvprx/domain/usecase/GetMergeListUseCase.kt
+++ app/src/main/java/com/omertex/mvprx/domain/usecase/GetMergeListUseCase.kt
@@ -2,24 +2,41 @@ package com.omertex.test.app.domain
 
 import com.omertex.mvprx.domain.repositories.OxTestRepository
 import com.omertex.mvprx.presentation.mvp.global.SchedulersProvider
+import com.omertex.test.app.data.model.MergeModel
+import com.omertex.test.app.data.model.MergeModelMapper
 import com.omertex.test.app.data.model.placeholder.User
 import com.omertex.test.app.data.model.unsplash.Photo
+import io.reactivex.rxjava3.core.Observable
 import io.reactivex.rxjava3.core.Single
+import io.reactivex.rxjava3.functions.BiFunction
 
 class GetMergeListUseCase(
     private val oxTestRepository: OxTestRepository,
+    private val mergeModelMapper: MergeModelMapper,
     private val schedulersProvider: SchedulersProvider
 ) :
-    BaseUseCase<Pair<List<User>, List<Photo>>> {
+    BaseUseCase<List<MergeModel>> {
 
-
-    override fun execute(): Single<Pair<List<User>, List<Photo>>> {
+    override fun execute(): Single<List<MergeModel>> {
         return oxTestRepository.getUsers()
-            .concatMap { users ->
+            .flatMap { users ->
                 oxTestRepository.getPhoto(users.size)
                     .map { photos ->
                         Pair(first = users, second = photos)
                     }
-            }.subscribeOn(schedulersProvider.io())
+            }
+            .flatMap {
+                Observable.zip(
+                        Observable.fromIterable(it.first),
+                        Observable.fromIterable(it.second),
+                        BiFunction<User,Photo, MergeModel> { t1, t2 ->
+                            mergeModelMapper.map(t1, t2)
+                        }
+                    )
+                    .subscribeOn(schedulersProvider.computation())
+                    .toList()
+            }
     }
 }
+
+
