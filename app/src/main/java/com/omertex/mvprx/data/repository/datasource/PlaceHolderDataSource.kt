48e5800cd1c94dab3313a4596e96c3973ba5aa36
Andrey Mishchenko <ant1kguild@gmail.com>
2020-03-20 23:37:36 +0300

    refactoring: add .subscribeOn(IO) on api coll

--- app/src/main/java/com/omertex/mvprx/data/repository/datasource/PlaceHolderDataSource.kt
+++ app/src/main/java/com/omertex/mvprx/data/repository/datasource/PlaceHolderDataSource.kt
@@ -1,10 +1,15 @@
 package com.omertex.test.app.data.datasource
 
 
+import com.omertex.mvprx.presentation.mvp.global.SchedulersProvider
 import com.omertex.test.app.data.datasource.api.PlaceHolderApi
 import com.omertex.test.app.data.model.placeholder.User
 import io.reactivex.rxjava3.core.Single
 
-class PlaceHolderDataSource(private val api: PlaceHolderApi) {
+class PlaceHolderDataSource(
+    private val api: PlaceHolderApi,
+    private val schedulersProvider: SchedulersProvider
+) {
     fun users(): Single<List<User>> = api.getUserList()
+        .subscribeOn(schedulersProvider.io())
 }
--- app/src/main/java/com/omertex/mvprx/data/repository/datasource/UnsplashDataSource.kt
+++ app/src/main/java/com/omertex/mvprx/data/repository/datasource/UnsplashDataSource.kt
@@ -1,8 +1,13 @@
 package com.omertex.test.app.data.datasource
 
 
+import com.omertex.mvprx.presentation.mvp.global.SchedulersProvider
 import com.omertex.test.app.data.datasource.api.UnsplashApi
 
-class UnsplashDataSource(private val api: UnsplashApi) {
+class UnsplashDataSource(
+    private val api: UnsplashApi,
+    private val schedulersProvider: SchedulersProvider
+) {
     fun photos(numberOfPhotos: Int) = api.getListPhotos(pageSize = numberOfPhotos)
+        .subscribeOn(schedulersProvider.io())
 }
